09e243bfa91d6606657461569cbde2c04f5d2725
Astoria Floyd <netadmin@broken-moon.net>
2021-08-06 18:39:33 -0500

    Fixes

--- rng.lua
+++ rng.lua
@@ -5,14 +5,15 @@ function rng.d20()
     tools.reseed()
     local dice = math.random(20)
     local messagePart1 = ('You rolled a ' .. dice .. ' out of 20')
-    messagepart2 = ""
+    print(messagePart1)
+    local messagepart2 = ''
     if dice==20 then
-         messagePart2 = ('Nat 20! Crititcal Hit')
+         messagepart2 = ('Nat 20! Crititcal Hit')
     else if dice==1 then
-         messagePart2 = ('Nat 1! Critical Shit!')
+         messagepart2 = ('Nat 1! Critical Shit!')
     end
     end
-    local message = messagePart1 .. '\n' .. messagePart2
+    local message = messagePart1 .. '\n' .. messagepart2
     return message
 end
 
